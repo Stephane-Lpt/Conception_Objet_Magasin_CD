d4db3b91317dd7f90ad25204d13586dfca3be6bb
Stephane Loppinet <stephaneloppinet@gmail.com>
2023-05-23 10:51:02 +0200

    Changement ComparateurArtiste

--- coo_td_intelliJ/src/ComparateurArtiste.java
+++ coo_td_intelliJ/src/ComparateurArtiste.java
@@ -4,6 +4,6 @@ public class ComparateurArtiste implements ComparateurCd {
 
     @Override
     public boolean etreAvant(CD cd1, CD cd2) {
-        return cd1.compareArtisteCD(cd2);
+        return c1.getNomArtiste().compareTo(cd2.getNomArtiste()) < 0;
     }
 }
